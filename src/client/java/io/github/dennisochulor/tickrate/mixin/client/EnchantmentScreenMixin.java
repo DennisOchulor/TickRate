c6f2bea04a4102d215ba2390d4b8a4d16549ea04
Dennis Ochulor <dennisnwanevu@gmail.com>
2024-12-14 10:40:45 +0800

    Fix EnchantmentScreen book animation

---
+++ src/client/java/io/github/dennisochulor/tickrate/mixin/client/EnchantmentScreenMixin.java
@@ -0,0 +1,18 @@
+package io.github.dennisochulor.tickrate.mixin.client;
+
+import io.github.dennisochulor.tickrate.TickRateClientManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EnchantmentScreen.class)
+public class EnchantmentScreenMixin {
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderTickCounter;getTickDelta(Z)F"))
+    public float render(RenderTickCounter instance, boolean ignoreFreeze) {
+        return TickRateClientManager.getEntityTickDelta(MinecraftClient.getInstance().player).tickDelta();
+    }
+}
