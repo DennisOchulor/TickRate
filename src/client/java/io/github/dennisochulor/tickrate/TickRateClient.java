fc2c3e987de250df4438c796e88b054681923995
Dennis Ochulor <dennisnwanevu@gmail.com>
2024-12-28 11:18:53 +0800

    update fabric.mod.json, use PNG icon instead of JPEG

--- src/client/java/io/github/dennisochulor/tickrate/TickRateClient.java
+++ src/client/java/io/github/dennisochulor/tickrate/TickRateClient.java
@@ -3,7 +3,6 @@ package io.github.dennisochulor.tickrate;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.text.Text;
