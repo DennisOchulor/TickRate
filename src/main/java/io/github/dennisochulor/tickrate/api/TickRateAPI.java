9883825ce0ab8dfc28e7693da162efe91e83973b
Dennis Ochulor <dennisnwanevu@gmail.com>
2025-01-23 15:23:27 +0800

    Flesh out specification

--- src/main/java/io/github/dennisochulor/tickrate/api/TickRateAPI.java
+++ src/main/java/io/github/dennisochulor/tickrate/api/TickRateAPI.java
@@ -9,10 +9,10 @@ import net.minecraft.world.World;
 import java.util.Collection;
 
 /**
- * API v0 for TickRate v0.3.0
- * <p>
+ * API v0 for TickRate v0.3.0 <p>
  * This class represents the sole entrypoint for TickRate's API. This API should only be used on the server-side.
  */
+ @SuppressWarnings("unused")
 public interface TickRateAPI {
 
     /**
@@ -34,45 +34,170 @@ public interface TickRateAPI {
      */
     float queryEntity(Entity entity);
 
+    /**
+     * Sets the tick rate of the specified entities. <code>rate</code> is rounded using {@link Math#round(float)}
+     *
+     * @throws IllegalArgumentException if {@link Entity#isRemoved()} is true for any of the specified entities.
+     */
     void rateEntity(Collection<? extends Entity> entities, float rate);
 
+    /**
+     * Sets the tick rate of the specified entity. <code>rate</code> is rounded using {@link Math#round(float)}
+     *
+     * @throws IllegalArgumentException if {@link Entity#isRemoved()} is true.
+     */
     void rateEntity(Entity entity, float rate);
 
+    /**
+     * Freezes or unfreezes the specified entities depending on <code>freeze</code>.
+     *
+     * @param freeze <code>true</code> to freeze, <code>false</code> to unfreeze
+     * @throws IllegalArgumentException if {@link Entity#isRemoved()} is true for any of the specified entities.
+     */
     void freezeEntity(Collection<? extends Entity> entities, boolean freeze);
 
+    /**
+     * Freezes or unfreezes the specified entity depending on <code>freeze</code>.
+     *
+     * @param freeze <code>true</code> to freeze, <code>false</code> to unfreeze
+     * @throws IllegalArgumentException if {@link Entity#isRemoved()} is true the specified entity.
+     */
     void freezeEntity(Entity entity, boolean freeze);
 
+    /**
+     * Steps the specified entities for <code>stepTicks</code> ticks. The entities will step according to their current TPS. <p>
+     * If <code>stepTicks</code> is 0, the entities will stop stepping.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> {@link Entity#isRemoved()} is true for any of the specified entities.
+     *          <li> Any of the specified entities are currently NOT frozen or ARE sprinting.
+     * </ul>
+     */
     void stepEntity(Collection<? extends Entity> entities, int stepTicks);
 
+    /**
+     * Steps the specified entity for <code>stepTicks</code> ticks. The entity will step according to its current TPS. <p>
+     * If <code>stepTicks</code> is 0, the entitiy will stop stepping.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> {@link Entity#isRemoved()} is true.
+     *          <li> The specified entity is currently NOT frozen or IS sprinting.
+     * </ul>
+     */
     void stepEntity(Entity entity, int stepTicks);
 
+    /**
+     * Sprints the specified entities for <code>sprintTicks</code> ticks. <p>
+     * If <code>sprintTicks</code> is 0, the entities will stop sprinting.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> {@link Entity#isRemoved()} is true for any of the specified entities.
+     *          <li> Any of the specified entities are currently stepping.
+     * </ul>
+     */
     void sprintEntity(Collection<? extends Entity> entities, int sprintTicks);
 
+    /**
+     * Sprints the specified entity for <code>sprintTicks</code> ticks. <p>
+     * If <code>sprintTicks</code> is 0, the entity will stop sprinting.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> {@link Entity#isRemoved()} is true.
+     *          <li> The specified entity is currently stepping.
+     * </ul>
+     */
     void sprintEntity(Entity entity, int sprintTicks);
+
 
 
     /**
      * Returns the tick rate of the chunk. <p>
      * If the chunk has no specific tick rate, the server's tick rate will be returned.
      *
-     * @throws IllegalArgumentException if the chunk's level type is {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE}.
+     * @throws IllegalArgumentException if the chunk is {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
      */
     float queryChunk(World world, ChunkPos chunk);
 
+    /**
+     * Sets the tick rate of the specified chunks. <code>rate</code> is rounded using {@link Math#round(float)}
+     *
+     * @throws IllegalArgumentException if any of the chunks are {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     */
     void rateChunk(Collection<? extends ChunkPos> chunks, float rate);
 
+    /**
+     * Sets the tick rate of the specified chunk. <code>rate</code> is rounded using {@link Math#round(float)}
+     *
+     * @throws IllegalArgumentException if the chunk is {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     */
     void rateChunk(ChunkPos chunk, float rate);
 
+    /**
+     * Freezes or unfreezes the specified chunks depending on <code>freeze</code>.
+     *
+     * @param freeze <code>true</code> to freeze, <code>false</code> to unfreeze
+     * @throws IllegalArgumentException if any of the chunks are {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     */
     void freezeChunk(Collection<? extends ChunkPos> chunks, boolean freeze);
 
+    /**
+     * Freezes or unfreezes the specified chunk depending on <code>freeze</code>.
+     *
+     * @param freeze <code>true</code> to freeze, <code>false</code> to unfreeze
+     * @throws IllegalArgumentException if the chunk is {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     */
     void freezeChunk(ChunkPos chunk, boolean freeze);
 
+    /**
+     * Steps the specified chunks for <code>stepTicks</code> ticks. The chunks will step according to their current TPS. <p>
+     * If <code>stepTicks</code> is 0, the chunks will stop stepping.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> Any of the chunks are {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     *          <li> Any of the specified chunks are currently NOT frozen or ARE sprinting.
+     * </ul>
+     */
     void stepChunk(Collection<? extends ChunkPos> chunks, int stepTicks);
 
+    /**
+     * Steps the specified chunk for <code>stepTicks</code> ticks. The chunk will step according to its current TPS. <p>
+     * If <code>stepTicks</code> is 0, the chunk will stop stepping.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> The chunk is {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     *          <li> The specified chunk is currently NOT frozen or IS sprinting.
+     * </ul>
+     */
     void stepChunk(ChunkPos chunk, int stepTicks);
 
+    /**
+     * Sprints the specified chunks for <code>sprintTicks</code> ticks. <p>
+     * If <code>sprintTicks</code> is 0, the chunks will stop sprinting.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> Any of the chunks are {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     *          <li> Any of the specified chunks are currently stepping.
+     * </ul>
+     */
     void sprintChunk(Collection<? extends ChunkPos> chunks, int sprintTicks);
 
+    /**
+     * Sprints the specified chunk for <code>sprintTicks</code> ticks. <p>
+     * If <code>sprintTicks</code> is 0, the chunk will stop sprinting.
+     *
+     * @throws IllegalArgumentException if any of the following conditions are met: <p>
+     * <ul>
+     *          <li> The chunk is {@link ChunkLevelType#INACCESSIBLE INACCESSIBLE} (not loaded).
+     *          <li> The specified chunk is currently stepping.
+     * </ul>
+     */
     void sprintChunk(ChunkPos chunk, int sprintTicks);
 
 }
--- src/main/java/io/github/dennisochulor/tickrate/api/TickRateEvents.java
+++ src/main/java/io/github/dennisochulor/tickrate/api/TickRateEvents.java
@@ -8,58 +8,88 @@ import net.minecraft.world.World;
 
 
 /**
- * TickRate related events.
+ * TickRate related events. <p>
+ * These events are fired when the associated <code>/tick</code> command is run and also when the associated
+ * API method is called. Registering event handlers for these events is identical to registering event handlers for Fabric events. <p>
+ *
+ * Unlike {@link TickRateAPI}, event handlers can be registered before the server has fully initialised.
  */
 @SuppressWarnings("unused")
 public interface TickRateEvents {
 
+    /**
+     * Called when the tick rate of an entity has been modified.
+     */
     Event<EntityRate> ENTITY_RATE = EventFactory.createArrayBacked(EntityRate.class, callbacks -> ((entity, rate) -> {
         for (EntityRate callback : callbacks) {
             callback.onEntityRate(entity, rate);
         }
     }));
 
+    /**
+     * Called when an entity is frozen or unfrozen.
+     */
     Event<EntityFreeze> ENTITY_FREEZE = EventFactory.createArrayBacked(EntityFreeze.class, callbacks -> ((entity, freeze) -> {
         for (EntityFreeze callback : callbacks) {
             callback.onEntityFreeze(entity, freeze);
         }
     }));
 
+    /**
+     * Called when an entity starts stepping.
+     */
     Event<EntityStep> ENTITY_STEP = EventFactory.createArrayBacked(EntityStep.class, callbacks -> ((entity, stepTicks) -> {
         for (EntityStep callback : callbacks) {
             callback.onEntityStep(entity, stepTicks);
         }
     }));
 
+    /**
+     * Called when an entity starts sprinting.
+     */
     Event<EntitySprint> ENTITY_SPRINT = EventFactory.createArrayBacked(EntitySprint.class, callbacks -> ((entity, sprintTicks) -> {
         for (EntitySprint callback : callbacks) {
             callback.onEntitySprint(entity, sprintTicks);
         }
     }));
 
+    /**
+     * Called when the tick rate of a chunk is modified.
+     */
     Event<ChunkRate> CHUNK_RATE = EventFactory.createArrayBacked(ChunkRate.class, callbacks -> ((world, chunkPos, rate) -> {
         for (ChunkRate callback : callbacks) {
             callback.onChunkRate(world, chunkPos, rate);
         }
     }));
 
+    /**
+     * Called when a chunk is frozen or unfrozen.
+     */
     Event<ChunkFreeze> CHUNK_FREEZE = EventFactory.createArrayBacked(ChunkFreeze.class, callbacks -> ((world, chunkPos, freeze) -> {
         for (ChunkFreeze callback : callbacks) {
             callback.onChunkFreeze(world, chunkPos, freeze);
         }
     }));
 
+    /**
+     * Called when a chunk starts stepping.
+     */
     Event<ChunkStep> CHUNK_STEP = EventFactory.createArrayBacked(ChunkStep.class, callbacks -> ((world, chunkPos, stepTicks) -> {
         for (ChunkStep callback : callbacks) {
             callback.onChunkStep(world, chunkPos, stepTicks);
         }
     }));
 
+    /**
+     * Called when a chunk starts sprinting.
+     */
     Event<ChunkSprint> CHUNK_SPRINT = EventFactory.createArrayBacked(ChunkSprint.class, callbacks -> ((world, chunkPos, sprintTicks) -> {
         for (ChunkSprint callback : callbacks) {
             callback.onChunkSprint(world, chunkPos, sprintTicks);
         }
     }));
+
+
 
     @FunctionalInterface
     interface EntityRate {
