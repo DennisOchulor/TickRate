852b03f63889572759bbbb7cf650651ddce7b2d5
Dennis Ochulor <dennisnwanevu@gmail.com>
2025-03-11 08:41:49 +0800

    more packaging

--- src/main/java/io/github/dennisochulor/tickrate/mixin/core/TickCommandMixin.java
+++ src/main/java/io/github/dennisochulor/tickrate/mixin/core/TickCommandMixin.java
@@ -1,4 +1,4 @@
-package io.github.dennisochulor.tickrate.mixin;
+package io.github.dennisochulor.tickrate.mixin.core;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.FloatArgumentType;
