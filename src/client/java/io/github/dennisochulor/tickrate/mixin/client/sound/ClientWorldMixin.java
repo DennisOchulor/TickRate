2795ea49d2a474c8753611463818be57c59c73e8
Dennis Ochulor <dennisnwanevu@gmail.com>
2025-03-23 14:26:32 +0800

    Update sound changes to 1.21.5

--- src/client/java/io/github/dennisochulor/tickrate/mixin/client/sound/ClientWorldMixin.java
+++ src/client/java/io/github/dennisochulor/tickrate/mixin/client/sound/ClientWorldMixin.java
@@ -7,9 +7,7 @@ import io.github.dennisochulor.tickrate.TickState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
@@ -17,7 +15,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ClientWorld.class)
@@ -28,8 +25,8 @@ public class ClientWorldMixin {
     /**
      * Entity pitch change is handled by EntityMixin (common side) already
      */
-    @ModifyArg(method = "playSound(Lnet/minecraft/entity/Entity;DDDLnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/sound/SoundCategory;FFJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZJ)V"), index = 6)
-    public float playSoundWithPlayer(float pitch, @Local(argsOnly = true, ordinal = 0) double x, @Local(argsOnly = true, ordinal = 1) double y, @Local(argsOnly = true, ordinal = 2) double z, @Local(argsOnly = true) SoundCategory category, @Local(argsOnly = true) RegistryEntry<SoundEvent> event) {
+    @ModifyVariable(method = "playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZJ)V", at = @At("HEAD"), argsOnly = true, ordinal = 1)
+    public float playSound(float pitch, @Local(argsOnly = true, ordinal = 0) double x, @Local(argsOnly = true, ordinal = 1) double y, @Local(argsOnly = true, ordinal = 2) double z, @Local(argsOnly = true) SoundCategory category) {
         return switch(category) {
             case MASTER,MUSIC,RECORDS,VOICE,NEUTRAL,HOSTILE -> pitch;
             case PLAYERS -> {
@@ -44,6 +41,32 @@ public class ClientWorldMixin {
             }
             case BLOCKS, AMBIENT -> {
                 TickState state = TickRateClientManager.getChunkState(ChunkPos.toLong(new BlockPos(MathHelper.floor(x), MathHelper.floor(y), MathHelper.floor(z))));
+                if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
+                else yield pitch * (state.rate() / 20.0F);
+            }
+        };
+    }
+
+    /**
+     * Entity pitch change is handled by EntityMixin (common side) already
+     * Entity is assumed to be MinecraftClient#player
+     */
+    @ModifyVariable(method = "playSoundClient(Lnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V", at = @At("HEAD"), argsOnly = true, ordinal = 1)
+    public float playSoundClient(float pitch, @Local(argsOnly = true) SoundCategory category) {
+        return switch(category) {
+            case MASTER,MUSIC,RECORDS,VOICE,NEUTRAL,HOSTILE -> pitch;
+            case PLAYERS -> {
+                TickState state = TickRateClientManager.getEntityState(client.player);
+                if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
+                else yield pitch * (state.rate() / 20.0F);
+            }
+            case WEATHER -> {
+                TickState state = TickRateClientManager.getServerState();
+                if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
+                else yield pitch * (state.rate() / 20.0F);
+            }
+            case BLOCKS, AMBIENT -> {
+                TickState state = TickRateClientManager.getChunkState(client.player.getChunkPos().toLong());
                 if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
                 else yield pitch * (state.rate() / 20.0F);
             }
@@ -57,26 +80,11 @@ public class ClientWorldMixin {
         else return pitch * (state.rate() / 20.0F);
     }
 
-    @ModifyVariable(method = "playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZ)V", at = @At("HEAD"), argsOnly = true, ordinal = 1)
-    public float playSound(float pitch, @Local(argsOnly = true, ordinal = 0) double x, @Local(argsOnly = true, ordinal = 1) double y, @Local(argsOnly = true, ordinal = 2) double z, @Local(argsOnly = true) SoundCategory category, @Local(argsOnly = true) SoundEvent event) {
-        return switch(category) {
-            case MASTER,MUSIC,RECORDS,VOICE,NEUTRAL,HOSTILE -> pitch;
-            case PLAYERS -> {
-                TickState state = TickRateClientManager.getEntityState(client.player);
-                if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
-                else yield pitch * (state.rate() / 20.0F);
-            }
-            case WEATHER -> {
-                TickState state = TickRateClientManager.getServerState();
-                if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
-                else yield pitch * (state.rate() / 20.0F);
-            }
-            case BLOCKS, AMBIENT -> {
-                TickState state = TickRateClientManager.getChunkState(ChunkPos.toLong(new BlockPos(MathHelper.floor(x), MathHelper.floor(y), MathHelper.floor(z))));
-                if(state.sprinting()) yield TickRate.MAX_SOUND_PITCH;
-                else yield pitch * (state.rate() / 20.0F);
-            }
-        };
+    @ModifyVariable(method = "playSoundFromEntityClient", at = @At("HEAD"), argsOnly = true, ordinal = 1)
+    public float playSoundFromEntityClient(float pitch, @Local(argsOnly = true) Entity entity) {
+        TickState state = TickRateClientManager.getEntityState(entity);
+        if(state.sprinting()) return TickRate.MAX_SOUND_PITCH;
+        else return pitch * (state.rate() / 20.0F);
     }
 
 }
--- src/main/java/io/github/dennisochulor/tickrate/mixin/sound/ServerWorldMixin.java
+++ src/main/java/io/github/dennisochulor/tickrate/mixin/sound/ServerWorldMixin.java
@@ -4,13 +4,10 @@ import com.llamalad7.mixinextras.sugar.Local;
 import io.github.dennisochulor.tickrate.TickRate;
 import io.github.dennisochulor.tickrate.TickState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.ServerTickManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
@@ -30,7 +27,7 @@ public class ServerWorldMixin {
      * Entity pitch change is handled by EntityMixin already
      */
     @ModifyVariable(method = "playSound", at = @At("HEAD"), argsOnly = true, ordinal = 1)
-    public float playSound(float pitch, @Local(argsOnly = true) Entity entity, @Local(argsOnly = true, ordinal = 0) double x, @Local(argsOnly = true, ordinal = 1) double y, @Local(argsOnly = true, ordinal = 2) double z, @Local(argsOnly = true) SoundCategory category, @Local(argsOnly = true) RegistryEntry<SoundEvent> event) {
+    public float playSound(float pitch, @Local(argsOnly = true) Entity entity, @Local(argsOnly = true, ordinal = 0) double x, @Local(argsOnly = true, ordinal = 1) double y, @Local(argsOnly = true, ordinal = 2) double z, @Local(argsOnly = true) SoundCategory category) {
         ServerTickManager tickManager = server.getTickManager();
         return switch(category) {
             case MASTER,MUSIC,RECORDS,VOICE,NEUTRAL,HOSTILE -> pitch;
