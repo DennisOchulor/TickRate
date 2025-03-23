611d5736510f0461d0d4ba50189f2782b80df52a
Dennis Ochulor <dennisnwanevu@gmail.com>
2025-03-23 20:06:09 +0800

    Fix InventoryScreen entity rendering issue WITHOUT using StackWalker

--- src/client/java/io/github/dennisochulor/tickrate/mixin/client/render/EntityRenderDispatcherMixin.java
+++ src/client/java/io/github/dennisochulor/tickrate/mixin/client/render/EntityRenderDispatcherMixin.java
@@ -2,7 +2,6 @@ package io.github.dennisochulor.tickrate.mixin.client.render;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import io.github.dennisochulor.tickrate.TickRateClientManager;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,10 +13,7 @@ public class EntityRenderDispatcherMixin {
 
     @ModifyVariable(method = "render(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), argsOnly = true)
     public float render(float tickProgress, @Local(argsOnly = true) Entity entity) {
-        if(StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).walk(stream -> stream.skip(2).findFirst().get()).getDeclaringClass() == InventoryScreen.class) {
-            return tickProgress; // this is a TERRIBLE way of coding, but I don't know how else to fix the entity rendering for the InventoryScreen
-        }
-        return TickRateClientManager.getEntityTickProgress(entity).tickProgress(); // tickProgress
+        return TickRateClientManager.getEntityTickProgress(entity).tickProgress();
     }
 
 }
