4314ba0b84897b0dc79505e53784c0b7880b4277
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-01-02 15:16:35 +0800

    target param by name

--- src/client/java/io/github/dennisochulor/tickrate/client/mixin/sound/ClientLevelMixin.java
+++ src/client/java/io/github/dennisochulor/tickrate/client/mixin/sound/ClientLevelMixin.java
@@ -16,7 +16,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
 import java.util.Objects;
 
 @Mixin(ClientLevel.class)
@@ -27,7 +26,7 @@ public class ClientLevelMixin {
     /**
      * Entity pitch change is handled by EntityMixin (common side) already
      */
-    @ModifyVariable(method = "playSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZJ)V", at = @At("HEAD"), argsOnly = true, ordinal = 1)
+    @ModifyVariable(method = "playSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZJ)V", at = @At("HEAD"), argsOnly = true, name = "pitch")
     public float playSound(float pitch, @Local(argsOnly = true, ordinal = 0) double x, @Local(argsOnly = true, ordinal = 1) double y, @Local(argsOnly = true, ordinal = 2) double z, @Local(argsOnly = true) SoundSource category) {
         Objects.requireNonNull(minecraft.player);
 
