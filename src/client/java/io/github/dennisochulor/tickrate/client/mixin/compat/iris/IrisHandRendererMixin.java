bba685f390f1ab5c2c7aa9781878c8af5d0d8650
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-06-17 09:21:33 +0800

    update to 26.2, update iris dep

--- src/client/java/io/github/dennisochulor/tickrate/client/mixin/compat/iris/IrisHandRendererMixin.java
+++ src/client/java/io/github/dennisochulor/tickrate/client/mixin/compat/iris/IrisHandRendererMixin.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 /**
  IRIS COMPATIBILITY - fixes player hand/held item stutter issue
  Iris uses the server's partialTick to render player hand, so give Iris the player's partialTick instead
- See <a href="https://github.com/IrisShaders/Iris/blob/26.1/common/src/main/java/net/irisshaders/iris/mixin/MixinLevelRenderer.java">here</a>
+ See <a href="https://github.com/IrisShaders/Iris/blob/26.2/common/src/main/java/net/irisshaders/iris/mixin/MixinLevelRenderer.java">here</a>
  */
 @Mixin(HandRenderer.class)
 public abstract class IrisHandRendererMixin {
